ec2c9cce13d5d8cb8b5d18bdbbe89e22b8c9792a
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-16 17:11:02 +0700

    lab10

---
+++ lab10/test/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//2 dimensional array: table
+	//1st dimension: row
+	//2nd dimension: column
+	int row = 2;
+	int col = 3;
+	double table[row][col];
+	//assign value for each element (cell in table)
+	table[0][0] = 1;
+	table[0][1] = 8;
+	table[0][2] = 10;
+	table[1][0] = 9;
+	table[1][1] = 5;
+	table[1][2] = 3;
+ 
+}
+ 
